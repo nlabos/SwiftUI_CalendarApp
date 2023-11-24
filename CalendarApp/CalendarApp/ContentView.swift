49754dffbef4ea5f147dd419fa942b31e85ddf2f
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2023-11-24 17:54:38 +0900

    カレンダーを追加

--- CalendarApp/CalendarApp/ContentView.swift
+++ CalendarApp/CalendarApp/ContentView.swift
@@ -10,19 +10,21 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
+    @State private var selectedDate = Date() // カレンダーで選択している日付を保持する変数
     @Query private var items: [Item]
-
+    
     var body: some View {
         NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+            VStack {
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .onChange(of: selectedDate) { _ in
+                        // Handle change in date selection
                     }
+                
+                List(filteredItems(for: selectedDate)) { item in
+                    Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                 }
-                .onDelete(perform: deleteItems)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -38,20 +40,25 @@ struct ContentView: View {
             Text("Select an item")
         }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
             modelContext.insert(newItem)
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
                 modelContext.delete(items[index])
             }
         }
+    }
+    
+    private func filteredItems(for date: Date) -> [Item] {
+        // Filter the `items` array to return only those items that match the selected date
+        return items.filter { Calendar.current.isDate($0.timestamp, inSameDayAs: date) }
     }
 }
 
